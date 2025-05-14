d9efa8ca591ccbbd90939de1f86ac53ff2761152
ACharanjith <155658324+ACharanjith@users.noreply.github.com>
2025-05-14 15:37:57 +0530

    Update sudoku.test.js

--- tests/sudoku.test.js
+++ tests/sudoku.test.js
@@ -1,3 +1,27 @@
-test('adds 1 + 1 to equal 2', () => {
-  expect(1 + 1).toBe(2);
+const { isValidMove } = require('./sudoku');
+
+describe('isValidMove', () => {
+  const emptyBoard = Array(9).fill(null).map(() => Array(9).fill(null));
+
+  test('allows a valid move', () => {
+    expect(isValidMove(emptyBoard, 0, 0, 5)).toBe(true);
+  });
+
+  test('blocks duplicate in row', () => {
+    const board = structuredClone(emptyBoard);
+    board[0][1] = 5;
+    expect(isValidMove(board, 0, 0, 5)).toBe(false);
+  });
+
+  test('blocks duplicate in column', () => {
+    const board = structuredClone(emptyBoard);
+    board[1][0] = 5;
+    expect(isValidMove(board, 0, 0, 5)).toBe(false);
+  });
+
+  test('blocks duplicate in box', () => {
+    const board = structuredClone(emptyBoard);
+    board[1][1] = 5;
+    expect(isValidMove(board, 0, 0, 5)).toBe(false);
+  });
 });
